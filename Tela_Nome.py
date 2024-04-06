35d5e53cd140be63b63904db48f9a83b756956b6
João Victor da Silva Araújo <jvsilvaaraujo98@gmail.com>
2024-04-06 11:38:04 -0300

    Tentativa de Mudança conforme ux

--- Tela_Nome.py
+++ Tela_Nome.py
@@ -13,6 +13,7 @@ db_config = {
     'autocommit': True,
 }
 
+
 def Nomes():
     conn = pymysql.connect(**db_config)
     query = "SELECT Nome FROM dClientes"
@@ -20,15 +21,16 @@ def Nomes():
     conn.close()
     return df
 
+
 def tela_inicial():
-    st.set_page_config(initial_sidebar_state="collapsed",page_title="Soninha Tech",  page_icon="🏪", menu_items={
+    st.set_page_config(initial_sidebar_state="collapsed", page_title="Soninha Tech",  page_icon="🏪", menu_items={
         'About': """Soninha Tech
         Feito por Data Analytics, caso queira suporte ou tenha alguma sugestão, entre em contato conosco.
         Envie um e-mail para data_analytics@grupotrigo.com.br
         """
     })
     st.markdown(
-    """
+        """
     <style>
     [data-testid="collapsedControl"] {
         display: none
@@ -43,21 +45,21 @@ def tela_inicial():
     }
     </style>
     """,
-    unsafe_allow_html=True,
+        unsafe_allow_html=True,
     )
     nomes = Nomes()
-    st.title("Ponto de Venda : Sonia Tech")
-    st.subheader("Bem Vindo ao PDV da Sonia, o Famoso Sonia Tech")
+    st.title("Delícias da Tia Sônia")
+    st.subheader("Bem Vindo a lojinha da Sonia, o famoso Sônia Tech")
     name_input = st.selectbox(
-    "Digite Seu Nome Abaixo:",
-    nomes, ## Lista de nomes (dClientes)
-    key='name',
-    index = None,
-    placeholder='Selecione seu nome'
+        "Digite Seu Nome Abaixo:",
+        nomes,  # Lista de nomes (dClientes)
+        key='name',
+        index=None,
+        placeholder='Selecione seu nome'
     )
-    
+
     col1, col2, col3, col4 = st.columns(4)
-    nome = st.session_state.name    
+    nome = st.session_state.name
     with col1:
         butao_compra = st.button("Fazer Compra", type="primary")
         if butao_compra and nome != None:
@@ -71,5 +73,7 @@ def tela_inicial():
         elif botao_pagemento and nome == None:
             st.error("Você não digitou um nome", icon="🚨")
     st.divider()
+
+
 if __name__ == "__main__":
     tela_inicial()
--- pages/Tela_Compra.py
+++ pages/Tela_Compra.py
@@ -16,18 +16,22 @@ db_config = {
 
 st.session_state.travaDuploClick = 0
 
+
 def Obtem_Preco_Banco():
     conn = pymysql.connect(**db_config)
     query = "SELECT * FROM dProdutos"
     df_precos = pd.read_sql(query, conn)
     conn.close()
-    df_precos = df_precos.sort_values('Data', ascending=False).drop_duplicates('Produto')
-    df_precos['Filtro'] = df_precos['Produto'] + ' - R$: ' + df_precos['Valor'].astype(str)
+    df_precos = df_precos.sort_values(
+        'Data', ascending=False).drop_duplicates('Produto')
+    df_precos['Filtro'] = df_precos['Produto'] + \
+        ' - R$: ' + df_precos['Valor'].astype(str)
     return df_precos
+
 
 def Tela_Compra():
     st.markdown(
-    """
+        """
     <style>
     [data-testid="collapsedControl"] {
         display: none
@@ -42,12 +46,12 @@ def Tela_Compra():
     }
     </style>
     """,
-    unsafe_allow_html=True,
+        unsafe_allow_html=True,
     )
 
     Verifica_Compras_No_Session_State()
     st.session_state.name = st.session_state.name
-    if "nomeimutavel" in st.session_state: ##foda!!
+    if "nomeimutavel" in st.session_state:  # foda!!
         nome = st.session_state.nomeimutavel
     else:
         nome = st.session_state.name
@@ -55,10 +59,9 @@ def Tela_Compra():
     if 'travaDuploClick' not in st.session_state:
         st.session_state.travaDuploClick = 0
 
-    st.title("Tela de Compra")
-    st.subheader(f"Bem - Vindo, {nome.title()} faça sua compra abaixo.")
-    col1,col2 = st.columns([2,1])
-    col_but1,col_but2,col_but3= st.columns(3)
+    st.title("Cardápio")
+    col1, col2 = st.columns([2, 1])
+    col_but1, col_but2, col_but3 = st.columns(3)
     use_category_filter = st.checkbox("Deseja procurar por categoria?")
     if use_category_filter:
         category_input = st.selectbox(
@@ -72,7 +75,8 @@ def Tela_Compra():
     with col1:
         product_input = st.selectbox(
             "Selecione o produto consumido",
-            df_precos['Filtro'] if not use_category_filter else df_precos.query("Categoria==@category_input")['Filtro'],
+            df_precos['Filtro'] if not use_category_filter else df_precos.query(
+                "Categoria==@category_input")['Filtro'],
             key='product',
             index=None,
             placeholder='Selecione o produto'
@@ -88,10 +92,6 @@ def Tela_Compra():
         )
     with col_but1:
         butao_comprar_mais = st.button("Adicionar ao carrinho", type='primary')
-    with col_but2:
-        butao_conclusao = st.button("Finalizar a Compra")
-    with col_but3:
-        butao_cancelar = st.button("Cancelar Compras")
     if butao_conclusao:
         if st.session_state.df_compras.empty:
             st.error("Você não cadastrou nenhuma compra!!!", icon="🚨")
@@ -101,31 +101,41 @@ def Tela_Compra():
         if product_input is not None and quantity_input != 0:
             st.session_state.Flag_Clicou_aqui = True
             Salva_Compra()
-            st.success(f"{st.session_state.quantity} {df_precos.loc[df_precos['Filtro'] == st.session_state.product, 'Produto'].iloc[0]} adicionado ao carrinho!")
+            st.success(
+                f"{st.session_state.quantity} {df_precos.loc[df_precos['Filtro'] == st.session_state.product, 'Produto'].iloc[0]} adicionado ao carrinho!")
         else:
             st.error("Você não selecionou nenhum produto!!!", icon="🚨")
     Escreve_Compras()
+    with col_but2:
+        butao_conclusao = st.button("Finalizar a Compra")
+    with col_but3:
+        butao_cancelar = st.button("Cancelar Compras")
     if butao_cancelar:
         Cancela_Compras()
         st.session_state.df_compras()
 
+
 def Salva_Compra():
     df_precos = Obtem_Preco_Banco()
     nome = st.session_state.name
-    produto = df_precos.loc[df_precos['Filtro'] == st.session_state.product, 'Produto'].iloc[0]
+    produto = df_precos.loc[df_precos['Filtro'] ==
+                            st.session_state.product, 'Produto'].iloc[0]
     quantidade = st.session_state.quantity
     preco = df_precos.query("Produto==@produto")['Valor'].iloc[0] * quantidade
-    
-    #Alteraçõo feita para corrigir o erro de append usando concat
-    nova_compra = pd.DataFrame({"Nome": [nome], "Produto": [produto], "Quantidade": [quantidade], "Preco": [preco]})
-    st.session_state.df_compras = pd.concat([st.session_state.df_compras, nova_compra], ignore_index=True)
-    
+
+    # Alteraçõo feita para corrigir o erro de append usando concat
+    nova_compra = pd.DataFrame({"Nome": [nome], "Produto": [
+                               produto], "Quantidade": [quantidade], "Preco": [preco]})
+    st.session_state.df_compras = pd.concat(
+        [st.session_state.df_compras, nova_compra], ignore_index=True)
+
     # Código estava dando erro pq não tem atributo append
     # AttributeError: 'DataFrame' object has no attribute 'append'
-    #st.session_state.df_compras = st.session_state.df_compras.append({"Nome":nome,
-     ##                                                               "Quantidade":quantidade,
-       #                                                              "Preco":preco},ignore_index=True)
-    
+    # st.session_state.df_compras = st.session_state.df_compras.append({"Nome":nome,
+    # "Quantidade":quantidade,
+    #                                                              "Preco":preco},ignore_index=True)
+
+
 def Escreve_Compras():
     st.subheader("Compras Registradas:")
     if not st.session_state.df_compras.empty:
@@ -133,17 +143,19 @@ def Escreve_Compras():
         Valor_Gasto = np.sum(st.session_state.df_compras['Preco'])
         st.subheader(f"Gasto Total **{Valor_Gasto:.2f}**")
     else:
-        st.text("Nenhuma compra registrada.")         
+        st.text("Nenhuma compra registrada.")
 
 
 def Verifica_Compras_No_Session_State():
     if 'df_compras' not in st.session_state:
-        st.session_state.df_compras = pd.DataFrame(columns=["Nome", "Produto", "Quantidade","Preco"])
+        st.session_state.df_compras = pd.DataFrame(
+            columns=["Nome", "Produto", "Quantidade", "Preco"])
         return st.session_state.df_compras
 
 
 def Cancela_Compras():
-    st.session_state.df_compras = pd.DataFrame(columns=["Nome", "Produto", "Quantidade","Preco"])
+    st.session_state.df_compras = pd.DataFrame(
+        columns=["Nome", "Produto", "Quantidade", "Preco"])
     switch_page("Tela_Nome")
 
 
