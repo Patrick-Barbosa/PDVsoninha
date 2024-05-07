8d293e918b8d3034884094a60c8511918543f7d7
João Victor da Silva Araújo <jvsilvaaraujo98@gmail.com>
2024-05-07 18:00:46 -0300

    atualizacao

--- pages/Tela_Conclusao.py
+++ pages/Tela_Conclusao.py
@@ -58,8 +58,7 @@ def Tela_Conclusao():
                              ["Sim", "Não"],
                              index=1)
 
-    col1, space, col2 = st.columns([1, 0.1, 1])
-
+    col1, col2, col3, col4 = st.columns(4)
 
     with col1:
         st.write(" ")
